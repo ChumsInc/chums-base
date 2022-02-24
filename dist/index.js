e3a213b845626d04577472e9fcbfa6f666acda80
Steve Montgomery <steve@chums.com>
2022-02-24 11:48:50 -0700

    changed type back to module, added .js extensions in index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
-export { apiFetch } from './api-fetch';
-export { sendEmail, sendGmail, getLogoImageAttachment, getTs, getTs36, sendOldSESEmail } from './mailer';
-export { mysql2Pool, mysql2Pool as pool, getConnection } from './mysql';
-export { execQuery, query } from './query';
+export { apiFetch } from './api-fetch.js';
+export { sendEmail, sendGmail, getLogoImageAttachment, getTs, getTs36, sendOldSESEmail } from './mailer.js';
+export { mysql2Pool, mysql2Pool as pool, getConnection } from './mysql.js';
+export { execQuery, query } from './query.js';
 export { default as sageOdbc, default as SageODBC } from './sage-odbc.js';
-export { resultToExcelSheet, buildXLSXHeaders, buildWorkBook, addResultToExcelSheet, parseDataForAOA } from './toXLSX';
-export { mysqlDate, getDBCompany, getSageCompany, parseSQL, validateAccountParams, validateARDivisionNo, validateCustomerNo } from './utils';
-export { validateUser, validateRole, loadValidation } from './validate-user';
-export { validateUserAccount, validateUserAccount as validateAccount } from './validate-user-account';
+export { resultToExcelSheet, buildXLSXHeaders, buildWorkBook, addResultToExcelSheet, parseDataForAOA } from './toXLSX.js';
+export { mysqlDate, getDBCompany, getSageCompany, parseSQL, validateAccountParams, validateARDivisionNo, validateCustomerNo } from './utils.js';
+export { validateUser, validateRole, loadValidation } from './validate-user.js';
+export { validateUserAccount, validateUserAccount as validateAccount } from './validate-user-account.js';
