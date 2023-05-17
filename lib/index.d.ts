5657bb322a86a4162cb835b8027bc3bf26dad572
Steve Montgomery <steve@chums.com>
2023-05-17 16:38:21 -0600

    fix: and finally rebuild ts files as well

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,6 +2,6 @@ export * from 'chums-user-validation';
 export * from './mailer.js';
 export * from './mysql.js';
 export * from './query.js';
-export * from './sage-odbc.js';
+export { default as sageOdbc, default as SageODBC, SageQueryResult } from './sage-odbc.js';
 export * from './toXLSX.js';
 export * from './utils.js';
