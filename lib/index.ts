bfbc9fa4dd25aef5e63a86fdfc0b21a1b9ed58c4
Steve Montgomery <steve@chums.com>
2023-05-17 16:34:41 -0600

    fix: export sageOdbc

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@ export * from 'chums-user-validation'
 export * from './mailer.js';
 export * from './mysql.js';
 export * from './query.js';
-export * from './sage-odbc.js';
+export {default as sageOdbc, SageQueryResult} from './sage-odbc.js';
 export * from './toXLSX.js';
 export * from './utils.js';
 
