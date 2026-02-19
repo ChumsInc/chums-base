fd82c68a4245cb45c73a76323df481c03c37af94
Steve Montgomery <steve@chums.com>
2026-02-19 13:22:19 -0700

    refactor: enhance typing and streamline utility functions in `toXLSX` module

--- lib/toXLSX.ts
+++ lib/toXLSX.ts
@@ -1,60 +1,63 @@
-import {Debug} from './debug.js';;
+import {Debug} from './debug.js';
 import {SheetAOAOpts, utils, WorkSheet, write, WritingOptions} from 'xlsx';
 import {RowDataPacket} from "mysql2";
 
 const debug = Debug('chums:chums-base:toXLSX');
 export const decode_cell = utils.decode_cell;
 export const encode_cell = utils.encode_cell;
+export const {aoa_to_sheet, json_to_sheet, sheet_add_json, sheet_add_aoa} = utils;
+export type {WorkSheet, WritingOptions, SheetAOAOpts} from 'xlsx';
 
-export interface ColumnNames {
-    [key: string]: string,
-}
+export type ColumnNames<T = RowDataPacket> = {
+    [key in keyof T]: string;
+};
 
 export interface WorkBookSheets {
     [key: string]: WorkSheet,
 }
 
-export function parseDataForAOA(data: RowDataPacket[], columnNames: ColumnNames, onlyColumnNames: boolean): any[][] {
-    let rows: any[] = [];
-    let fields: string[] = [];
-    let columns: string[] = [];
+export function parseDataForAOA<T = RowDataPacket>(data: T[], columnNames: ColumnNames<T>, onlyColumnNames: boolean): unknown[][] {
+    let fields: (keyof T)[] = [];
     if (onlyColumnNames) {
-        fields = Object.keys(columnNames);
-        columns = [...fields];
+        fields = Object.keys(columnNames) as (keyof T)[];
         if (data.length) {
             fields.forEach(field => {
                 if (data[0][field] === undefined) {
-                    debug('resultToExcelSheet()', `field '${field}' does not exist in data.`);
+                    debug('resultToExcelSheet()', `field '${String(field)}' does not exist in data.`);
                 }
             })
         }
-        rows = [
-            columns.map(col => columnNames[col] || col),
-            ...data.map(row => columns.map(col => row[col] || null))
-        ];
-    } else {
-        if (data.length) {
-            fields = Object.keys(data[0]);
-            columns = [...fields];
-            rows = [
-                columns.map(col => columnNames[col] || col),
-                ...data.map(row => Object.values(row))
-            ];
-        }
+        return [
+            fields.map(field => columnNames[field] ?? field),
+            ...data.map(row => {
+                return fields.map(field => row[field] ?? null);
+            })
+        ]
     }
-    return rows;
+
+    if (data.length) {
+        fields = Object.keys(data[0] as unknown as object) as (keyof T)[];
+        return [
+            fields,
+            ...data.map(row => {
+                return fields.map(field => row[field] ?? null);
+            })
+        ]
+    }
+
+    return [];
 }
 
-export function resultToExcelSheet(data: RowDataPacket[], columnNames: ColumnNames, onlyColumnNames: boolean): WorkSheet {
-    let rows: any[] = parseDataForAOA(data, columnNames, onlyColumnNames);
+export function resultToExcelSheet<T = RowDataPacket>(data: T[], columnNames: ColumnNames<T>, onlyColumnNames: boolean): WorkSheet {
+    const rows = parseDataForAOA<T>(data, columnNames, onlyColumnNames);
     return utils.aoa_to_sheet(rows);
 }
 
-export function addResultToExcelSheet(workSheet: WorkSheet, newData: any[][], options: SheetAOAOpts): WorkSheet {
+export function addResultToExcelSheet<T = RowDataPacket>(workSheet: WorkSheet, newData: T[][], options: SheetAOAOpts): WorkSheet {
     return utils.sheet_add_aoa(workSheet, newData, options);
 }
 
-export function buildWorkBook(sheets: WorkBookSheets, options: WritingOptions = {}): any {
+export function buildWorkBook(sheets: WorkBookSheets, options: WritingOptions = {}): unknown {
     const defaultOptions: WritingOptions = {
         type: 'buffer',
         cellDates: false,
@@ -67,11 +70,11 @@ export function buildWorkBook(sheets: WorkBookSheets, options: WritingOptions = 
     return write({SheetNames: sheetNames, Sheets: sheets}, {...defaultOptions, ...options});
 }
 
-export function buildXLSXHeaders(filename: string): { 'Content-Disposition': string, 'Content-Type': string } {
-    return {
-        'Content-Disposition': `attachment; filename=${filename.replace(/[\s]+/g, '_')}`,
-        'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    }
+export function buildXLSXHeaders(filename: string): Headers {
+    const headers = new Headers();
+    headers.set('Content-Disposition', `attachment; filename=${filename.replace(/[\s]+/g, '_')}`);
+    headers.set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    return headers;
 }
 
 
